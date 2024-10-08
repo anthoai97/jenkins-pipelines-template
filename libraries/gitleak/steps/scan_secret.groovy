ff330d1186a63eacd2b22e208ef681931a7c7eed
An Quach <anquach.dev@gmail.com>
2024-10-08 14:04:07 +0700

    Test 2

--- libraries/gitleak/steps/scan_secret.groovy
+++ libraries/gitleak/steps/scan_secret.groovy
@@ -1,12 +1,11 @@
 void call() {
     stage('Setup Gitleaks') {
-        script {
-            def gitleaksInstalled = sh(returnStatus: true, script: 'which gitleaks > /dev/null') == 0
-                
-            if (!gitleaksInstalled) {
-                    sh 'mkdir -p bin'
-                    sh 'curl -sfL https://install.goreleaser.com/github.com/zricethezav/gitleaks.sh | sh -s -- -b bin'                }
-            env.PATH = "${env.WORKSPACE}/bin:${env.PATH}"
+        def gitleaksInstalled = sh(returnStatus: true, script: 'which gitleaks > /dev/null') == 0
+            
+        if (!gitleaksInstalled) {
+                sh 'mkdir -p bin'
+                sh 'curl -sfL https://install.goreleaser.com/github.com/zricethezav/gitleaks.sh | sh -s -- -b bin'                
         }
+        env.PATH = "${env.WORKSPACE}/bin:${env.PATH}"
     }
 }
